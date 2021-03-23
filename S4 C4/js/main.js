fd47e4271a7ea2173f63c7f0ac4a945f525ebcb0
towerstt <towerstt3@gmail.com>
2021-03-22 21:41:42 -0600

    S5 C1

---
+++ S4 C4/js/main.js
@@ -0,0 +1,13 @@
+alert("Hola yo!!!");
+console.log("¡Hola Mundo! JS")
+
+var n1 = 10;
+var n2 = 3;
+
+console.log(n1+n2);
+console.log(n1-n2);
+console.log(n1*n2);
+console.log(n1/n2);
+console.log(n1%n2);
+console.log(++n1);
+console.log(--n2);
---
+++ S5 C1/main.js
@@ -0,0 +1,54 @@
+
+// //Ejercicio 1
+// var sideOne = parseInt(prompt("Ingresa el valor del primer lado"));
+// var sideTwo = parseInt(prompt("Ingresa el valor del segundo lado"));
+// var sideThree = parseInt(prompt("Ingresa el valor del tercer lado"));
+
+// if(sideOne === sideTwo && sideOne === sideThree){
+//     alert("El triángulo es equilátero");
+// }
+// else if(sideOne === sideTwo || sideOne === sideThree || sideTwo === sideThree){
+//     alert("El triángulo es isóceles");
+// }
+// else{
+//     alert("El triángulo es un triángulo escaleno");
+// }
+
+
+
+// //Ejercicio 2
+// var sOne = parseInt(prompt("Ingresa el valor del primer lado"));
+// var sTwo = parseInt(prompt("Ingresa el valor del segundo lado"));
+// var areaC = sOne * sTwo;
+
+// if(sOne === sTwo){
+//     alert("La figura es un cuadrado y su área es " + areaC);
+// }
+// else{
+//     alert("La figura es un rectángulo y su área es " + areaC);
+// }
+
+// //Ejercicio 3
+// var base = parseInt(prompt("Ingresa el valor de la base del triángulo: "));
+// var heigth = parseInt(prompt("Ingresa el valor de la altura del triángulo: "));
+// var areaT = (base * heigth)/2;
+// alert("El área de tu triángulo es de " + areaT);
+
+//Ejericio 4
+
+//condición ? true : false
+
+//Ejercicio 4.1
+var sideOne = parseInt(prompt("Ingresa el valor del primer lado"));
+var sideTwo = parseInt(prompt("Ingresa el valor del segundo lado"));
+var sideThree = parseInt(prompt("Ingresa el valor del tercer lado"));
+
+sideOne === sideTwo && sideOne === sideThree ? alert("El triángulo es equilátero") : sideOne === sideTwo || sideOne === sideThree || sideTwo === sideThree ? alert("El triángulo es isóceles") : alert("El triángulo es escaleno");
+
+//Ejercicio 4.2
+var sOne = parseInt(prompt("Ingresa el valor del primer lado"));
+var sTwo = parseInt(prompt("Ingresa el valor del segundo lado"));
+var areaC = sOne * sTwo;
+
+sOne === sideTwo ? alert(`La figura es un cuadrado y su área es de ${areaC}`) : alert("La figura es un rectángulo y su área es de :" + areaC);
+
