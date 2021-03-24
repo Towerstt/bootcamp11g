fe691e81b548157f6a351172c79810b4e1bb224f
towerstt <towerstt3@gmail.com>
2021-03-24 00:49:25 -0600

    S5 C2

--- S5 C2/main.js
+++ S5 C2/main.js
@@ -1,32 +1,39 @@
-/*
-    var phrase = "La mejor forma de predecir el futuro es creándolo"
-    1- saber la cantidad de palabras
-    2- saber la cantidad de "a"
-    3- crear una nueva frase usando sólo los caracteres que esten en posiciones nones
-        "amjrf"
-    4- crear una nueva frase usando sólo los caracteres que esten en posiciones pares
-*/
-
 var phrase = "La mejor forma de predecir el futuro es creándolo";
-
-//1
-
+//1.- Saber la cantidad de palabras
 console.log(`La frase tiene ${phrase.length} caracteres.`);
-
-//2
-var numOfA = 0;
+//2.- Saber la cantidad de "a"
+console.log(`La frase tiene ${phrase.match(/[aá]/gi).length} letras a`);
+//3.- Crear una nueva frase usando sólo los caracteres que esten en posiciones nones
+//4.- Crear una nueva frase usando sólo los caracteres que esten en posiciones pares
+var odd = '';
+var even = '';
 for (let i = 0; i < phrase.length; i++) {
-    if (phrase[i] === 'a' || phrase[i] === 'á') {
-        numOfA += 1;       
-    }
+    i%2 == 0 ? odd += phrase.charAt(i) : even += phrase.charAt(i);
 }
-console.log(`La frase tiene ${numOfA} letras a`);
-
-//3
-var odd = [];
-var even = [];
+console.log(odd);
+console.log(even);
+//-------------------------------------TAREA-------------------------------------
+//1.- Mostrar la misma frase en snake_case
+var snakeCase = '';
 for (let i = 0; i < phrase.length; i++) {
-    i%2 == 0 ? odd.push(phrase[i]): even.push(phrase[i]);
+    snakeCase = phrase.replace(/ /g, '_');    
 }
-console.log(odd.join())
-console.log(even.join())
+console.log(snakeCase);
+//2.- Mostrar la misma frase en kebab-case
+var kebabCase = '';
+for (let i = 0; i < phrase.length; i++) {
+    kebabCase = phrase.replace(/ /g, '-');    
+}
+console.log(kebabCase);
+//3.- Mostrar la misma frase con todas las vocales en mayúscula
+var upperCase = '';
+for (let i = 0; i < phrase.length; i++) {
+    upperCase = phrase.replace(/a|e|i|o|u|á|é|í|ó|ú/gi, function(x){return x.toUpperCase()})
+}
+console.log(upperCase);
+//4.- Mostrar los primeros 10 caracteres de la frase
+var tenPhrase = phrase.slice(0,10);
+console.log(tenPhrase);
+//5.- Mostrar los últimos 10 caracteres de la frase
+var phraseTen = phrase.slice(-10);
+console.log(phraseTen);
