5202a445669ea3f5306c47bd195d5f50566157bd
towerstt <towerstt3@gmail.com>
2021-03-26 19:33:07 -0600

    S5 C4T

--- S5 C4/main.js
+++ S5 C4/main.js
@@ -77,71 +77,114 @@ var productsList = [{name:"producto 1",
         ]
 */
 
-//quiero conocer la cantidad de productos en el array
-alert(`La cantidad de productos en tu lista de compra es de ${productsList.length}`);
-//quiero conocer el costo total de todos los productos del array
-let totalCost = 0;
-for (let i = 0; i < productsList.length; i++) {
-    totalCost += productsList[i].price;
-    console.log(productsList[i].price);
-}
-alert(`El costo total de todos los productos de la lista es de $${totalCost}`);
-//quiero conocer la cantidad de productos de cada categoría (Vegetables, Cloths, Miscellaneous, Computers)
-//quiero conocer el costo por categoría
-let numVegetables = 0, 
-    costVegetables = 0,
-    numCloths = 0,
-    costCloths = 0, 
-    numMiscellaneous = 0,
-    costMiscellaneous = 0, 
-    numComputers = 0,
-    costComputers = 0,
-    arrayVeg = [],
-    arrayClot = [],
-    arrayMisc = [],
-    arrayComp = [];
-for (let i = 0; i < productsList.length; i++) {
-    switch (productsList[i].category) {
-        case "Vegetables":
-            numVegetables += 1;
-            costVegetables += productsList[i].price;
-            arrayVeg.push(Object.values(productsList[i]));
-            break;
-        case 'Cloths':
-            numCloths += 1;
-            costCloths += productsList[i].price;
-            arrayClot.push(Object.values(productsList[i]));
-            break;
-        case 'Miscellaneous':
-            numMiscellaneous += 1;
-            costMiscellaneous += productsList[i].price;
-            arrayMisc.push(Object.values(productsList[i]));
-            break;
-        case 'Computers':
-            numComputers += 1;
-            costComputers += productsList[i].price;
-            arrayComp.push(Object.values(productsList[i]));
-            break;
-        default:
-            alert("Revisa la información")
-            break;
-    }        
-}
-alert(`Vegetables: ${numVegetables} con un costo de $${costVegetables}, 
-       Cloths: ${numCloths} con un costo de $${costCloths},
-       Miscellaneous: ${numMiscellaneous} con un costo de $${costMiscellaneous},
-       Computers: ${numComputers} con un costo de $${costComputers}`);
+// //quiero conocer la cantidad de productos en el array
+// alert(`La cantidad de productos en tu lista de compra es de ${productsList.length}`);
+// //quiero conocer el costo total de todos los productos del array
+// let totalCost = 0;
+// for (let i = 0; i < productsList.length; i++) {
+//     totalCost += productsList[i].price;
+//     console.log(productsList[i].price);
+// }
+// alert(`El costo total de todos los productos de la lista es de $${totalCost}`);
+// //quiero conocer la cantidad de productos de cada categoría (Vegetables, Cloths, Miscellaneous, Computers)
+// //quiero conocer el costo por categoría
+// let numVegetables = 0, 
+//     costVegetables = 0,
+//     numCloths = 0,
+//     costCloths = 0, 
+//     numMiscellaneous = 0,
+//     costMiscellaneous = 0, 
+//     numComputers = 0,
+//     costComputers = 0,
+//     arrayVeg = [],
+//     arrayClot = [],
+//     arrayMisc = [],
+//     arrayComp = [];
+// for (let i = 0; i < productsList.length; i++) {
+//     switch (productsList[i].category) {
+//         case "Vegetables":
+//             numVegetables += 1;
+//             costVegetables += productsList[i].price;
+//             arrayVeg.push(Object.values(productsList[i]));
+//             break;
+//         case 'Cloths':
+//             numCloths += 1;
+//             costCloths += productsList[i].price;
+//             arrayClot.push(Object.values(productsList[i]));
+//             break;
+//         case 'Miscellaneous':
+//             numMiscellaneous += 1;
+//             costMiscellaneous += productsList[i].price;
+//             arrayMisc.push(Object.values(productsList[i]));
+//             break;
+//         case 'Computers':
+//             numComputers += 1;
+//             costComputers += productsList[i].price;
+//             arrayComp.push(Object.values(productsList[i]));
+//             break;
+//         default:
+//             alert("Revisa la información")
+//             break;
+//     }        
+// }
+// alert(`Vegetables: ${numVegetables} con un costo de $${costVegetables}, 
+//        Cloths: ${numCloths} con un costo de $${costCloths},
+//        Miscellaneous: ${numMiscellaneous} con un costo de $${costMiscellaneous},
+//        Computers: ${numComputers} con un costo de $${costComputers}`);
 
-//quiero un nuevo array para cada categoría
-console.log(arrayVeg)
-console.log(arrayClot)
-console.log(arrayMisc)
-console.log(arrayComp )
+// //quiero un nuevo array para cada categoría
+// console.log(arrayVeg)
+// console.log(arrayClot)
+// console.log(arrayMisc)
+// console.log(arrayComp )
 
-//quiero un nuevo array de strings que tenga lo siguiente
-function objToString(array){
-    for (let i = 0; i < array.length; i++) {
-        alert(`El ${array[i].name} tiene un costo de $${array[i].price}`) 
-    }
-}
-objToString(productsList)
+// //quiero un nuevo array de strings que tenga lo siguiente
+// function objToString(array){
+//     for (let i = 0; i < array.length; i++) {
+//         alert(`El ${array[i].name} tiene un costo de $${array[i].price}`) 
+//     }
+// }
+// objToString(productsList)
+
+
+//Ejercicio 2.- TAREA
+//forEach, map, reduce, filter
+
+//1.- quiero conocer la cantidad de productos en el array
+const numOfPdts = array1 => array1.length
+let nOfP = numOfPdts(productsList)
+console.log(`La lista tiene ${nOfP} elementos`);
+//2.- quiero conocer el costo total de todos los productos del array
+let totCost = 0; totCost0 = 0; totCost1 = 0;
+const costcalc = array1 => array1.forEach(element => totCost += element.price);
+totalCost2= costcalc(productsList);
+alert(totalCost2);
+
+
+
+//3.- quiero conocer la cantidad de productos de cada categoría (Vegetables, Cloths, Miscellaneous, Computers)
+const categ = (array1, cat) => array1.filter(array1 => array1.category === cat)
+let veg = categ(productsList, "Vegetables");
+let clo = categ(productsList, "Cloths");
+let mis = categ(productsList, "Miscellaneous");
+let com = categ(productsList, "Computers");
+console.log(veg.length);
+console.log(clo.length);
+console.log(mis.length);
+console.log(com.length);
+
+//4.- quiero conocer el costo por categoría
+totCostVeg = costcalc(veg);
+totCostClo = costcalc(clo);
+totCostMis = costcalc(mis);
+totCostCom = costcalc(com);
+
+//5.- quiero un nuevo array para cada categoría
+console.log(veg);
+console.log(clo);
+console.log(mis);
+console.log(com);
+
+//6.- quiero un nuevo array de strings que tenga lo siguiente ["El { productName } tiene un costo de { price }"]
+const toStr = (array1) => array1.forEach( element => console.log(`El ${ element.name } tiene un costo de ${ element.price }`))
+toStr(productsList);
