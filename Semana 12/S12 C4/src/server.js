f1b7427020b6c1f9d817071b40938f060329c835
towerstt <towerstt3@gmail.com>
2021-05-21 21:55:16 -0500

    S12 C5

--- Semana 12/S12 C4/src/server.js
+++ Semana 12/S12 C4/src/server.js
@@ -3,11 +3,12 @@
 const express = require('express')
 const kodersRouter = require('./routers/koders')
 const mentorsRouter = require('./routers/mentors')
+const middleware = require('./middlewares/koders')
 const app = express()
 
+app.use('/', middleware)
 app.use('/koders', kodersRouter)
 app.use('/mentors', mentorsRouter)
-
 
 
 
