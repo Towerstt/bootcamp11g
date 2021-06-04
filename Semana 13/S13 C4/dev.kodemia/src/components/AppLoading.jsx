1cb4bb9216727eb87da5c264a51d85a222029333
towerstt <towerstt3@gmail.com>
2021-06-03 20:26:38 -0500

    S15 C4 1.3

--- Semana 13/S13 C4/dev.kodemia/src/components/AppLoading.jsx
+++ Semana 13/S13 C4/dev.kodemia/src/components/AppLoading.jsx
@@ -1,32 +1,32 @@
 import React, { useState } from "react";
 import Styles from "../styles/AppLoading.module.css";
 
-class AppLoading extends React.Component {
-  constructor(props) {
-    super(props);
-    this.props = props;
+// class AppLoading extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.props = props;
 
-    this.state = { active: true };
-  }
-  render() {
-    const active = this.state.active ? Styles.active : "";
-    return (
-      <div
-        className={`${Styles.loading} ${active} align-items-center justify-content-center`}
-      >
-        <div className="spinner-border" role="status">
-          <span className="visually-hidden">Loading...</span>
-        </div>
-      </div>
-    );
-  }
+//     this.state = { active: true };
+//   }
+//   render() {
+//     const active = this.state.active ? Styles.active : "";
+//     return (
+//       <div
+//         className={`${Styles.loading} ${active} align-items-center justify-content-center`}
+//       >
+//         <div className="spinner-border" role="status">
+//           <span className="visually-hidden">Loading...</span>
+//         </div>
+//       </div>
+//     );
+//   }
 
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({ active: false });
-    }, 100);
-  }
-}
+//   componentDidMount() {
+//     setTimeout(() => {
+//       this.setState({ active: false });
+//     }, 100);
+//   }
+// }
 
 //Refactorizando el componente como clase a componente como función
 
--- Semana 13/S13 C4/dev.kodemia/src/components/AppThemeToggle.jsx
+++ Semana 13/S13 C4/dev.kodemia/src/components/AppThemeToggle.jsx
@@ -1,26 +1,26 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 // import Styles from '../styles/AppTheme.module.css'
-import '../styles/AppTheme.css'
+import "../styles/AppTheme.css";
 
 // class AppThemeToggle extends React.Component{
 //     constructor(props){
 //         super(props)
 
 //         this.state = {theme : 'Light'};
-//     } 
-    
+//     }
+
 //     toggle() {
 //         const theme = this.state.theme === 'Light' ? 'Dark' : 'Light'
 
 //         this.setState({theme : theme})
 
 //     }
-    
+
 //     render(){ //Se crea el componente
 //         let styleToApply
 //         this.state.theme === 'Light' ? styleToApply = Styles.appThemeToggle : styleToApply = Styles.appThemeDark
 //         return(
-//             <div 
+//             <div
 //                 className={styleToApply}
 //                 onClick={this.toggle.bind(this)}
 //             >
@@ -38,65 +38,57 @@ import '../styles/AppTheme.css'
 //         document.body.setAttribute('theme', this.state.theme)
 //     }
 
-
-
-    // //HOOKS del LIFECYCLE
-    // componentWillMount(){ //Se ejecuta antes de que se monte en el DOM
-    //     const now = new Date()
-    //     console.log('antes de que se esté en el DOM!', now.getTime())
-    // }
-    // componentDidMount(){ //SE ejecuta justo después de que se monta en el DOM7
-    //     const now = new Date()
-    //     console.log('ya estoy en el DOM!', now.getTime())
-    // }
-    // componentWillUpdate(){ //Se ejecuta cada de que hay una actualización
-    //     const now = new Date()
-    //     console.log('antes de que me actualice!', now.getTime())
-    // }
-    // componentDidUpdate(){ //Se ejecuta justo después de actualizarse
-    //     const now = new Date()
-    //     console.log('después de actualizar,e!', now.getTime())
-    // }
-    // componentWillUnmount(){ //Se ejecuta antes de que el componente desaparezca
-    //     const now = new Date()
-    //     console.log('antes de que me quite del DOM!', now.getTime())
-    // }
+// //HOOKS del LIFECYCLE
+// componentWillMount(){ //Se ejecuta antes de que se monte en el DOM
+//     const now = new Date()
+//     console.log('antes de que se esté en el DOM!', now.getTime())
 // }
-
+// componentDidMount(){ //SE ejecuta justo después de que se monta en el DOM7
+//     const now = new Date()
+//     console.log('ya estoy en el DOM!', now.getTime())
+// }
+// componentWillUpdate(){ //Se ejecuta cada de que hay una actualización
+//     const now = new Date()
+//     console.log('antes de que me actualice!', now.getTime())
+// }
+// componentDidUpdate(){ //Se ejecuta justo después de actualizarse
+//     const now = new Date()
+//     console.log('después de actualizar,e!', now.getTime())
+// }
+// componentWillUnmount(){ //Se ejecuta antes de que el componente desaparezca
+//     const now = new Date()
+//     console.log('antes de que me quite del DOM!', now.getTime())
+// }
+// }
 
 //Refactorizar el componente como clase al componente como función
 
+function AppThemeToggle2(props) {
+  const [theme, setCounter] = useState("Light");
 
-function AppThemeToggle2 (props){
+  useEffect(() => {
+    document.body.setAttribute("theme", theme);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
+  useEffect(() => {
+    document.body.setAttribute("theme", theme);
+  }, [theme]);
 
-    const [theme, setCounter] = useState('Light')
-
-    useEffect(() =>{            
-        document.body.setAttribute('theme' , theme)
-    },[])
-
-    useEffect(() =>{            
-        document.body.setAttribute('theme', theme)
-    },[theme])
-
-
-    const toggle2 = (action) =>{
-
-        let themeMode = theme === 'Light' ? 'Dark' : 'Light'
-        setCounter(themeMode)
-    }
-    let styleToApply
-    theme === 'Light' ? styleToApply = 'appThemeToggle' : styleToApply = 'appThemeDark'
-    console.log(styleToApply)
-    return(
-        <div 
-            className={styleToApply}
-            onClick={toggle2}
-        >
-            {theme}
-        </div>
-    )
+  const toggle2 = (action) => {
+    let themeMode = theme === "Light" ? "Dark" : "Light";
+    setCounter(themeMode);
+  };
+  let styleToApply;
+  theme === "Light"
+    ? (styleToApply = "appThemeToggle")
+    : (styleToApply = "appThemeDark");
+  console.log(styleToApply);
+  return (
+    <div className={styleToApply} onClick={toggle2}>
+      {theme}
+    </div>
+  );
 }
 
-export default AppThemeToggle2
+export default AppThemeToggle2;
